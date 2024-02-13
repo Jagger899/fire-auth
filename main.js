48a70b1eed4f4c8f0dbd1c4cbd33e6fe687a5551
Jagger899 <romanskarb2@ukr.net>
2024-02-13 17:50:35 +0300

    first setting

---
+++ main.js
@@ -0,0 +1,21 @@
+// import { initializeApp } from "firebase/app";
+
+// // TODO: Replace the following with your app's Firebase project configuration
+// const firebaseConfig = {
+//   //...
+// };
+
+// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+// const app = initializeApp(firebaseConfig);
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// // Get a list of cities from your database
+// async function getCities(db) {
+//   const citiesCol = collection(db, "cities");
+//   const citySnapshot = await getDocs(citiesCol);
+//   const cityList = citySnapshot.docs.map((doc) => doc.data());
+//   return cityList;
+// }
---
+++ public/js/home.js
@@ -0,0 +1,3 @@
+const mailContainer = document.querySelector('.mail-container');
+const shownContainer = 'container mail-container shown-container';
+const hiddenMailContainer = "container mail-container hidden-container";
